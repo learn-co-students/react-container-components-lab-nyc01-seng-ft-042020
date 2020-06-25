52ff867769fac369b85702634a3d60fb84d0b334
blobbyblobfish <aywang6@gmail.com>
2020-06-25 00:10:31 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+
+import React from 'react'
+
+export default function MovieReviews(props) {
+
+    return ( 
+        <div className="review-list">
+            {props.reviews.map(review => { return (
+                <React.Fragment>
+                    <h1>{review.display_title}</h1>
+                    <p>{review.summary_short}</p>
+                </React.Fragment> )
+                })
+            }
+        </div>
+    )
+    
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
+import { replace } from 'sinon';
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        searchTerm: "",
+        reviews: []
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch(`${URL}?query=${this.state.searchTerm}`)
+            .then(resp => resp.json())
+            .then(json => this.setState({ reviews: json.results }))
+    }
+
+    render() {
+        return (
+        <React.Fragment>
+            <form className="searchableMovieReviewsContainer" onSubmit={this.handleSubmit}>
+                <input type="text" name="searchTerm" value={this.state.search} onChange={this.handleChange}></input>
+                <input type="submit"></input>
+            </form>
+            <h2>Searched Reviews:</h2>
+            <MovieReviews reviews={this.state.reviews}/>
+        </React.Fragment>
+        )
+    }
+}
